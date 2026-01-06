fa929fac7117c5cfc6705367888e44443fccd85f
Michael Lujan <m.lujan@striveworks.us>
2026-01-06 10:30:02 -0600

    removing unnecessary files

--- lujan-hack/build.sh
+++
@@ -1,3 +0,0 @@
-DATE=$(date '+%Y-%m-%d')
-FULL_TAG=724664234782.dkr.ecr.us-east-1.amazonaws.com/library/hardened/seldonio/mlserver:1.6.1-sklearn-${DATE}
-podman build -t ${FULL_TAG} .
--- strive-build.sh
+++ strive-build.sh
@@ -10,6 +10,6 @@ poetry lock
 
 version=$(sed 's/^__version__ = "\(.*\)"/\1/' ./mlserver/version.py)
 tag=${version}-sklearn-$(date '+%Y-%m-%d')
-# ./hack/build-image.sh ${version} sklearn || true
+./hack/build-image.sh ${version} sklearn || true
 podman tag localhost/seldonio/mlserver:${version}-sklearn 724664234782.dkr.ecr.us-east-1.amazonaws.com/library/hardened/seldonio/mlserver:${tag}
 podman push 724664234782.dkr.ecr.us-east-1.amazonaws.com/library/hardened/seldonio/mlserver:${tag}
